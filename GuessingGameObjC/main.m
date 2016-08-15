31db57beee13799f3f7bcbac7ffe983a562a8bd7
abwhitley <a.blakewhitley@gmail.com>
2016-08-15 18:46:35 -0400

    guessing game using obj c

--- GuessingGameObjC/main.m
+++ GuessingGameObjC/main.m
@@ -9,9 +9,30 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    int userNumber = -1;
+    int randomNumber = arc4random_uniform(20);
+    NSMutableArray *mutableArray = [NSMutableArray array];
+
+    for(int i = 0; i >= 0; i++){
+        while(randomNumber != userNumber){
+            NSNumber *number = @(randomNumber);
+            NSLog(@"Enter a number: ");
+            //NSLog(@"The random number is: %@", number);
+            fpurge(stdin);
+            scanf("%d", &userNumber);
+            NSNumber *guess = @(userNumber);
+
+            if(number == guess){
+                printf("You guessed correctly!");
+                return 1;
+            }else{
+                printf("Try again");
+                [mutableArray insertObject:guess atIndex:i];
+                
+                NSLog(@"Your guesses are: %@", mutableArray);
+            
+            }
+    }
     }
     return 0;
 }
